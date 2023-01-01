1a6673017689b0b2125a31bcb236b614f61fc068
priyanka <bharadwajpriyanka204@gmail.com>
2023-01-01 07:10:08 -0800

    emi calculator

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:emicalculator/home.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      )
+  );
+}
